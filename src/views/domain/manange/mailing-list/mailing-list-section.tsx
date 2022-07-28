d1d7f8d448d4dc5dda4bf2c0c51aeaf04952c5c2
zextras_kapil <kapil.nadiyapara@zextras.com>
2022-07-28 18:31:41 +0530

    fix: mailing list with query check of ldap

--- src/views/domain/manange/mailing-list/mailing-list-section.tsx
+++ src/views/domain/manange/mailing-list/mailing-list-section.tsx
@@ -3,13 +3,21 @@
  *
  * SPDX-License-Identifier: AGPL-3.0-only
  */
-import React, { FC, useCallback, useContext, useState } from 'react';
-import { Container, Text, Input, Row, Switch, Icon } from '@zextras/carbonio-design-system';
+import React, { FC, useCallback, useContext, useEffect, useMemo, useState } from 'react';
+import {
+	Container,
+	Text,
+	Input,
+	Row,
+	Switch,
+	Icon,
+	Table,
+	IconButton
+} from '@zextras/carbonio-design-system';
 import { useTranslation } from 'react-i18next';
-import { debounce } from 'lodash';
 import { MailingListContext } from './mailinglist-context';
 import ListRow from '../../../list/list-row';
-import { getDomainList } from '../../../../services/search-domain-service';
+import { searchDirectory } from '../../../../services/search-directory-service';
 
 const MailingListSection: FC<any> = () => {
 	const { t } = useTranslation();
@@ -17,13 +25,75 @@ const MailingListSection: FC<any> = () => {
 	const [domainList, setDomainList] = useState([]);
 
 	const { mailingListDetail, setMailingListDetail } = context;
-
+	const [dynamicListMember, setDynamicListMember] = useState<Array<any>>([]);
+	const [dynamicListMemberRows, setDynamicListMemberRows] = useState<Array<any>>([]);
 	const changeResourceDetail = useCallback(
 		(e) => {
 			setMailingListDetail((prev: any) => ({ ...prev, [e.target.name]: e.target.value }));
 		},
 		[setMailingListDetail]
 	);
+
+	const memberHeaders: any[] = useMemo(
+		() => [
+			{
+				id: 'members',
+				label: t('label.accounts', 'Accounts'),
+				width: '100%',
+				bold: true
+			}
+		],
+		[t]
+	);
+
+	const getMemberFromLdapQuery = useCallback(() => {
+		const query = mailingListDetail?.memberURL.replace('ldap:///??sub?', '');
+		searchDirectory(
+			'cn,description,name,zimbraId',
+			'accounts,distributionlists,dynamicgroups,accounts,aliases,dynamicgroups,resources',
+			'',
+			query
+		)
+			.then((response) => response.json())
+			.then((data) => {
+				const allList: any[] = [];
+				const account = data?.Body?.SearchDirectoryResponse?.account;
+				const dl = data?.Body?.SearchDirectoryResponse?.dl;
+				const alias = data?.Body?.SearchDirectoryResponse?.alias;
+				const calresource = data?.Body?.SearchDirectoryResponse?.calresource;
+
+				if (dl) {
+					dl.map((item: any) => allList.push({ id: item?.id, name: item?.name }));
+				}
+				if (account) {
+					account.map((item: any) => allList.push({ id: item?.id, name: item?.name }));
+				}
+				if (alias) {
+					alias.map((item: any) => allList.push({ id: item?.id, name: item?.name }));
+				}
+				if (calresource) {
+					calresource.map((item: any) => allList.push({ id: item?.id, name: item?.name }));
+				}
+				if (allList && allList.length > 0) {
+					setDynamicListMember(allList);
+				}
+			});
+	}, [mailingListDetail?.memberURL]);
+
+	useEffect(() => {
+		if (dynamicListMember && dynamicListMember.length > 0) {
+			const searchDlRows = dynamicListMember.map((item: any) => ({
+				id: item?.name,
+				columns: [
+					<Text size="medium" weight="bold" key={item?.id} color="#828282">
+						{item?.name}
+					</Text>,
+					''
+				]
+			}));
+			setDynamicListMemberRows(searchDlRows);
+		}
+	}, [dynamicListMember]);
 
 	return (
 		<Container mainAlignment="flex-start">
@@ -136,9 +206,38 @@ const MailingListSection: FC<any> = () => {
 								size="medium"
 								inputName="memberURL"
 								onChange={changeResourceDetail}
+								CustomIcon={(): any => (
+									<Icon
+										icon="CheckmarkOutline"
+										size="large"
+										color="grey"
+										onClick={getMemberFromLdapQuery}
+									/>
+								)}
 							/>
 						</Container>
 					</ListRow>
+				)}
+
+				{mailingListDetail?.dynamic && (
+					<>
+						<Row>
+							<Text
+								size="small"
+								mainAlignment="flex-start"
+								crossAlignment="flex-start"
+								orientation="horizontal"
+								weight="bold"
+							>
+								{t('label.members', 'Members')}
+							</Text>
+						</Row>
+						<ListRow>
+							<Container padding={{ top: 'large' }}>
+								<Table rows={dynamicListMemberRows} headers={memberHeaders} showCheckbox={false} />
+							</Container>
+						</ListRow>
+					</>
 				)}
 				<ListRow>
 					<Container
