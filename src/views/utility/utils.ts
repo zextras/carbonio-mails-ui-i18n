45084cc025dfbaf1c10254411320231d972e1cf7
divyang-dolphin <divyang@dolphinwebsolution.com>
2022-10-13 17:44:16 +0530

    fix: translation confirmed sproxy

--- src/views/utility/utils.ts
+++ src/views/utility/utils.ts
@@ -999,7 +999,7 @@ export const volumeAllocationList = (t: TFunction): Array<{ label: string; value
 		value: 2
 	},
 	{
-		label: t('volume.volume_allocation_list.proxy', 'Proxy'),
+		label: t('volume.volume_allocation_list.sproxy', 'Sproxy'),
 		value: 3
 	},
 	{
